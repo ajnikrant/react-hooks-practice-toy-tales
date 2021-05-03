dd125cf5ea69122530efd592c74c3346ce1561c7
Amanda <ajnikrant@gmail.com>
2021-05-03 19:31:48 -0400

    commit tree aa26e6cb3777cce1a4650ffa487c1e0dc3a4ab63

--- src/components/ToyContainer.js
+++ src/components/ToyContainer.js
@@ -1,9 +1,12 @@
 import React from "react";
 import ToyCard from "./ToyCard";
 
-function ToyContainer() {
+function ToyContainer({toys, deleteToy, updateToyLikes}) {
+
+const renderToys = toys.map((toy) => <ToyCard updateToyLikes={updateToyLikes} key={toy.id} toyObj={toy} deleteToy={deleteToy}/>)
+
   return (
-    <div id="toy-collection">{/* Render the collection of ToyCards */}</div>
+    <div id="toy-collection">{renderToys}</div>
   );
 }
 
--- src/components/ToyForm.js
+++ src/components/ToyForm.js
@@ -1,12 +1,45 @@
-import React from "react";
+import React, {useState} from "react";
 
-function ToyForm() {
+function ToyForm({renderNewToy}) {
+  const [formValue, setFormValue] = useState({
+    name:"",
+    image:""
+    })
+
+    function onFormChange(event){
+      setFormValue({...formValue, 
+      [event.target.name]: event.target.value
+      })
+
+      console.log(formValue)
+    }
+
+    function handleSubmit(){
+      const newToy={
+        name: formValue.name, 
+        image: formValue.image, 
+        likes:0
+      }
+
+      fetch('http://localhost:3001/toys', {
+        method: "POST", 
+        headers: {"Content-Type": "application/json"}, 
+        body: JSON.stringify(newToy)
+      })
+      .then(res => res.json())
+      .then(renderNewToy)
+      
+
+    }
+
   return (
     <div className="container">
-      <form className="add-toy-form">
+      <form className="add-toy-form" onSubmit={handleSubmit}>
         <h3>Create a toy!</h3>
         <input
           type="text"
+          onChange={onFormChange}
+          value={formValue.name}
           name="name"
           placeholder="Enter a toy's name..."
           className="input-text"
@@ -14,6 +47,8 @@ function ToyForm() {
         <br />
         <input
           type="text"
+          onChange={onFormChange}
+          value={formValue.image}
           name="image"
           placeholder="Enter a toy's image URL..."
           className="input-text"
